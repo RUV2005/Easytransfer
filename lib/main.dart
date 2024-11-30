a169d1181d9e3c252155492ccc5d2e123f6614ac
danmo <2099246846@qq.com>
2024-11-30 15:21:19 +0800

    The folder display is completed, but the bug that the file cannot be displayed needs to be fixed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 
 void main() {
@@ -20,14 +23,14 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.deepPurple,
+              seedColor: Colors.blue,
               brightness: Brightness.light,
             ),
             useMaterial3: true,
           ),
           darkTheme: ThemeData(
             colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.deepPurple,
+              seedColor: Colors.blue,
               brightness: Brightness.dark,
             ),
             useMaterial3: true,
@@ -67,7 +70,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
 
-  // 页面列表
   final List<Widget> _pages = const [
     Center(child: Text('主页', style: TextStyle(fontSize: 24))),
     Center(child: Text('文件传输', style: TextStyle(fontSize: 24))),
@@ -85,7 +87,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // 控制状态栏样式
     final isDarkMode = MediaQuery.of(context).platformBrightness == Brightness.dark;
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
@@ -97,46 +98,25 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     return Scaffold(
-      appBar: AppBar(title: const Text('')),  // 这里设置为空字符串
+      appBar: AppBar(title: const Text('')),
       body: SafeArea(
         child: _pages[_selectedIndex],
       ),
-      bottomNavigationBar: Container(
-        color: Colors.transparent, // 设置为透明
-        child: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.dashboard),
-              label: '主页',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.upload_file),
-              label: '文件传输',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.download),
-              label: '接收文件',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.share),
-              label: '剪切板共享',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.folder),
-              label: '文件管理',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: '设置',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-          selectedItemColor: Colors.deepPurple,
-          unselectedItemColor: Colors.grey,
-          elevation: 0, // 去除阴影
-          backgroundColor: Colors.transparent, // 设置为透明
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.dashboard), label: '主页'),
+          BottomNavigationBarItem(icon: Icon(Icons.upload_file), label: '文件传输'),
+          BottomNavigationBarItem(icon: Icon(Icons.download), label: '接收文件'),
+          BottomNavigationBarItem(icon: Icon(Icons.share), label: '剪切板共享'),
+          BottomNavigationBarItem(icon: Icon(Icons.folder), label: '文件管理'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: '设置'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
       ),
     );
   }
@@ -151,18 +131,80 @@ class FileManager extends StatefulWidget {
 
 class _FileManagerState extends State<FileManager> {
   List<FileSystemEntity> _files = [];
+  bool _isLoading = true;
+  late String _appFolderPath;
 
   @override
   void initState() {
     super.initState();
-    _listFiles();
+    _requestStoragePermission();
+  }
+
+  Future<void> _requestStoragePermission() async {
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      await Permission.storage.request();
+      status = await Permission.storage.status;
+    }
+    if (status.isGranted) {
+      _appFolderPath = '${Directory('/storage/emulated/0/Download').path}/Easytrasfer'; // 应用文件夹路径
+      await _createAppFolder(); // 创建文件夹
+      _listFiles(); // 列出文件
+    } else {
+      print('Storage permission denied');
+    }
+  }
+
+  Future<void> _createAppFolder() async {
+    final directory = Directory(_appFolderPath);
+    if (!await directory.exists()) {
+      await directory.create(recursive: true);
+      print('App folder created at: $_appFolderPath');
+    }
   }
 
   Future<void> _listFiles() async {
-    final Directory directory = Directory('/sdcard');
-    setState(() {
-      _files = directory.listSync();
-    });
+    final directory = Directory(_appFolderPath);
+    if (await directory.exists()) {
+      List<FileSystemEntity> entities = await directory.list().toList();
+      setState(() {
+        _files = entities.where((entity) {
+          String name = entity.path.split('/').last;
+          return !name.startsWith('.') && !name.isEmpty;
+        }).toList()
+          ..sort((a, b) => a.path.split('/').last.compareTo(b.path.split('/').last));
+        _isLoading = false;
+      });
+    } else {
+      print('Directory does not exist');
+    }
+  }
+
+  Future<void> _pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      final filePath = result.files.single.path!;
+      final file = File(filePath);
+      final newFile = await file.copy('$_appFolderPath/${file.uri.pathSegments.last}'); // 复制到应用专用文件夹
+      setState(() {
+        _files.add(newFile);
+      });
+    } else {
+      print('No file selected');
+    }
+  }
+
+  void _openDirectory(FileSystemEntity entity) {
+    if (entity is Directory) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => FileManagerPage(directory: entity),
+        ),
+      ).then((_) {
+        _listFiles(); // 返回时重新列出文件
+      });
+    }
   }
 
   @override
@@ -170,15 +212,74 @@ class _FileManagerState extends State<FileManager> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('文件管理'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _pickFile,
+          ),
+        ],
       ),
-      body: ListView.builder(
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
         itemCount: _files.length,
         itemBuilder: (context, index) {
           final file = _files[index];
           return ListTile(
             title: Text(file.path.split('/').last),
-            onTap: () {
-              // 在此处添加点击文件的逻辑
+            leading: Icon(file is Directory ? Icons.folder : Icons.insert_drive_file),
+            onTap: () => _openDirectory(file),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class FileManagerPage extends StatelessWidget {
+  final Directory directory;
+
+  const FileManagerPage({Key? key, required this.directory}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(directory.path.split('/').last)),
+      body: FutureBuilder<List<FileSystemEntity>>(
+        future: directory.list().toList(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          final files = snapshot.data!
+              .where((entity) => !entity.path.split('/').last.startsWith('.'))
+              .toList()
+            ..sort((a, b) => a.path.split('/').last.compareTo(b.path.split('/').last));
+
+          return ListView.builder(
+            itemCount: files.length,
+            itemBuilder: (context, index) {
+              final file = files[index];
+              return ListTile(
+                title: Text(file.path.split('/').last),
+                leading: Icon(file is Directory ? Icons.folder : Icons.insert_drive_file),
+                onTap: () {
+                  if (file is Directory) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => FileManagerPage(directory: file),
+                      ),
+                    );
+                  } else {
+                    print('Opening file: ${file.path}');
+                  }
+                },
+              );
             },
           );
         },
@@ -195,45 +296,115 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  bool _followSystemTheme = true; // 默认跟随系统主题
+  bool _followSystemTheme = true;
+  bool _notificationsEnabled = true;
+  String _selectedLanguage = 'English';
 
   @override
   void initState() {
     super.initState();
-    _followSystemTheme = ThemeNotifier.instance.value; // 获取当前状态
+    _followSystemTheme = ThemeNotifier.instance.value;
+  }
+
+  void _resetSettings() {
+    setState(() {
+      _followSystemTheme = true;
+      _notificationsEnabled = true;
+      _selectedLanguage = 'English';
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('设置'),
+        title: const Text('设置', style: TextStyle(fontSize: 20)),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(16.0),
-        children: [
-          SwitchListTile(
-            title: const Text('剪切板共享'),
-            value: false, // 这里可以根据实际需求调整
-            onChanged: (bool value) {
-              // 在此处添加逻辑以更改剪切板共享功能
-            },
-          ),
-          const Divider(),
-          SwitchListTile(
-            title: const Text('跟随系统切换主题'),
-            value: _followSystemTheme,
-            onChanged: (bool value) {
-              setState(() {
-                _followSystemTheme = value;
-                ThemeNotifier.instance.saveSettings(_followSystemTheme); // 保存设置
-              });
-            },
-          ),
-          const Divider(),
-          // 其他设置项...
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: ListView(
+          padding: const EdgeInsets.all(8.0),
+          children: [
+            SwitchListTile(
+              title: const Text('跟随系统切换主题'),
+              value: _followSystemTheme,
+              onChanged: (bool value) {
+                setState(() {
+                  _followSystemTheme = value;
+                  ThemeNotifier.instance.saveSettings(_followSystemTheme);
+                });
+              },
+            ),
+            const Divider(),
+            SwitchListTile(
+              title: const Text('启用通知'),
+              value: _notificationsEnabled,
+              onChanged: (bool value) {
+                setState(() {
+                  _notificationsEnabled = value;
+                });
+              },
+            ),
+            const Divider(),
+            ListTile(
+              title: const Text('选择语言'),
+              subtitle: Text(_selectedLanguage),
+              onTap: () {
+                _showLanguageDialog();
+              },
+            ),
+            const Divider(),
+            TextButton(
+              onPressed: _resetSettings,
+              child: const Text('重置所有设置'),
+            ),
+            const Divider(),
+          ],
+        ),
       ),
+    );
+  }
+
+  void _showLanguageDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('选择语言'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                ListTile(
+                  title: const Text('English'),
+                  onTap: () {
+                    setState(() {
+                      _selectedLanguage = 'English';
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: const Text('中文'),
+                  onTap: () {
+                    setState(() {
+                      _selectedLanguage = '中文';
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: const Text('取消'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
