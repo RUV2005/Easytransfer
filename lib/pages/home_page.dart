0ea3d3f898ca564df33f3af3929b08d0730e14fa
danmo <2099246846@qq.com>
2024-12-03 08:37:48 +0800

    feat: Implement swipe navigation in MyHomePage
    - Added PageView to allow users to swipe between Transfer, TransferFiles, and Settings pages.
    - Integrated PageController to manage page transitions.
    - Updated BottomNavigationBar to allow page switching via taps.
    
    This change improves the user experience by enabling both swipe and tap navigation methods.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,7 +4,7 @@ import 'file_select_page.dart'; // 确保导入
 import 'settings_page.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -12,6 +12,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
+  final PageController _pageController = PageController(); // 添加 PageController
 
   final List<Widget> _pages = const [
     TransferPage(),
@@ -19,18 +20,29 @@ class _MyHomePageState extends State<MyHomePage> {
     SettingsPage(),
   ];
 
-  void _onItemTapped(int index) {
+  void _onPageChanged(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
+  void _onItemTapped(int index) {
+    _pageController.jumpToPage(index); // 使用 PageController 跳转到指定页面
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose(); // 释放资源
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('')),
-      body: SafeArea(
-        child: _pages[_selectedIndex],
+      body: PageView(
+        controller: _pageController, // 设置 PageController
+        onPageChanged: _onPageChanged, // 监听页面变化
+        children: _pages,
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
@@ -39,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
           BottomNavigationBarItem(icon: Icon(Icons.settings), label: '设置'),
         ],
         currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
+        onTap: _onItemTapped, // 点击导航栏时调用
         selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.grey,
         elevation: 0,
